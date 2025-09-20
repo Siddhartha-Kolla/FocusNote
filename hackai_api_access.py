314f4d5351fea77710c580070212345abbc07b8f
Arian Malekzadeh <arian.malekzadeh@campus.tu-berlin.de>
2025-09-20 18:50:32 +0200

    add exam_gen!

--- hackai_api_access.py
+++ hackai_api_access.py
@@ -11,7 +11,7 @@ class HackAPI():
         content_raw : str = message["content"].split("</think>")
         content_assistant : str = content_raw[0].replace("<think>","").strip()
         content_response : str = content_raw[1].strip() if len(content_raw) > 1 else ""
-
+    
         return content_assistant, content_response
 
     def get_text_from_hackai_response(user_prompt: str = "Do nothing", assistant: str = "You are a helpful assistant") -> str:
@@ -24,6 +24,8 @@ class HackAPI():
                             ],
             },
         )
+        if response.status_code != 200:
+            raise Exception(f"HackAI API request failed with status code {response.status_code}")
         assistant, response = HackAPI.extract_text_from_hackai_response(response.json())
         return response
 
